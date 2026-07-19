121a542d1601fa862d22888bf8417894e4535fc9
zsm5026 <153550826+zsm5026@users.noreply.github.com>
2026-07-19 19:05:40 -0400

    Fix photo path casing and move image into src for passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/Assets");
+
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return new Date(dateObj).toLocaleDateString("en-US", {
       year: "numeric",
